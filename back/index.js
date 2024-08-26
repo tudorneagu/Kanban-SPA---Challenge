2e08cf483d88319338ce3f70bcfe4a7516ccd4ef
tudorneagu <neagu.t@gmail.com>
2024-08-26 22:59:46 +0200

    prisma setup completed

--- back/index.js
+++ back/index.js
@@ -1,11 +1,30 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-const express = require("express");
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+async function main() {
+  const allUsers = await prisma.tasks.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
